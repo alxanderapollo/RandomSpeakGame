1225e3ca8cec96455d0d54518c4720ada5ede93d
alxanderapollo <alxanderapollo@gmail.com>
2021-04-03 16:33:11 -0400

    set up a board to display the randome text

--- RandomSpeak.gd
+++ RandomSpeak.gd
@@ -8,3 +8,13 @@ func _ready():
 	var moviePrompt = ["alex", "Matrix", "amazing", "art"]
 	var game2 = "Once upon a time %s saw the %s and thought it was an %s peice of %s  "
 	print(game2 % moviePrompt)
+	
+	#get node function
+	#Fetches a node.
+	#get_node("DisplayText")
+	#another way to get a node and modify its properties
+	#what we are doing is grabbing a Label node which i renamed to display text
+	# we are then filling its text feild with this is some text
+	$DisplayText.text = "This is some text"
+	$DisplayText.text = game2 % moviePrompt
+		
