25d449423b2fb1f4f96dd57795210a7cca27cfa8
alxanderapollo <alxanderapollo@gmail.com>
2021-04-03 20:54:02 -0400

    added a functional button

--- RandomSpeak.gd
+++ RandomSpeak.gd
@@ -15,6 +15,33 @@ func _ready():
 	#another way to get a node and modify its properties
 	#what we are doing is grabbing a Label node which i renamed to display text
 	# we are then filling its text feild with this is some text
-	$DisplayText.text = "This is some text"
-	$DisplayText.text = game2 % moviePrompt
+	#$DisplayText.text = "This is some text"
+	#since VBOX is dictating the diaply of the nodes within the contianer
+	# i need to get the relative path for the game to correctly display the contents 
+	$VBoxContainer/DisplayText.text = game2 % moviePrompt
 		
+
+#this is what is known as a signal it allows us to input/output with the text box
+#this function takes in, input from the user and returns it and
+#places it on our display TEXT Node
+#after each input the text box is clear and so is the input section
+#for the user to type in
+func _on_PlayerText_text_entered(text):
+	updateDisplayText(text)
+	
+
+
+#signal for when the usr presses the button
+func _on_TextureButton_pressed():
+	#hold the words that are inputted by the user
+	var words = $VBoxContainer/HBoxContainer/PlayerText.text
+	#pass it up to the function
+	updateDisplayText(words)
+
+#signal for when the usr enters text into the information
+func updateDisplayText(text):
+	$VBoxContainer/DisplayText.text = text
+	#clears the text after each input from the user
+	$VBoxContainer/HBoxContainer/PlayerText.clear()
+	
+	
